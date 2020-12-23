c494f6452f41697a75a05b30a92fd530108ef639
Rimesh <rimeshlwagun10@gmail.com>
2020-12-23 12:15:11 +0545

    Fixed Empty List #2

--- Codes/Python/main.py
+++ Codes/Python/main.py
@@ -5,7 +5,8 @@ import glob
 #created
 import processing
 
-path = '.' 	#later, we will change to data dir. which is only for data 
+path = '../../dataset/RL/mentally' 	#later, we will change to data dir. which is only for data 
+# path = '/home/rimesh/Files/major/codes/AlterEgo-master/Core'
 sampling_frequency = 250	#	Hz
 
 #load data
@@ -13,44 +14,79 @@ def dataset(**kwargs):
 	data_dir = path
 	filepaths = glob.glob(path + "/**/*.txt", recursive= True)
 	filepaths = filepaths[:3]
-	# kwargs = 
 	return [processing.process(1, [file], labels = file.split('/')[-2],**kwargs) for file in filepaths]
 
 total_data = dataset(channels = range(0, 8), surrounding=210)
 
 
-def filtered_data(data):
+from scipy import signal
+def filter_data(data):
+	#this follows the arnav process
+
 	#signal processing
 	#	- bpf, notch filter (50 Hz)
-	from scipy import signal
 	
-	#applying bandpass filter
-	filter_order = 10
-	critical_frequencies = [15, 50] #in Hz
-	FILTER = 'bandpass'
+	#applying high pass filter - 0.5, used to remove frequencies lower than 0.5Hz
+	filter_order = 1
+	# critical_frequencies = [15, 50] #in Hz
+	critical_frequency = 0.5 	# in Hz
+	FILTER = 'highpass'				#'bandpass'
+	output = 'sos'
+	#design butterworth bandpass filter
+	sos = signal.butter(filter_order, critical_frequency, FILTER, fs = sampling_frequency, output= output)
+	filtered = signal.sosfilt(sos, data)
+
+	#normalize -(normalizing to a mean amplitude of zero (still need to cross check this))
+	data = data - np.mean(data, axis = 0)
+
+	#3rd order notch (butterworth// need to implement still) - power line noise , 50 Hz and its harmonics.
+	#applying notch filter
+	notch_times = 3
+	notch_frequency = 50 	#Hz
+	quality_factor = 30 	# 			-- no reason just copied.
+	
+	#design notch filter 
+	# b, a = signal.iirnotch(notch_frequency, quality_factor, sampling_frequency)
+	# filtered = signal.lfilter(b, a, filtered)
+	
+	freqs = list(map(int , list(map(round, np.arange(1, sampling_frequency/(2. * notch_frequency))* notch_frequency ))  ))
+	for _ in range(notch_times):
+		for f in reversed(freqs):
+			b, a = signal.iirnotch(f, quality_factor, sampling_frequency)
+			filtered = signal.lfilter(b, a, filtered)
+
+
+	#TODO: removing heartbeat artifacts...
+	#applying ricker
+	# widths = np.arange(1, 50)
+	# cwtmatr = signal.cwt(data,signal.ricker, widths)
+	# data = data - cwtmatr n
+
+
+	#applying bandpass filter, 0.5 - 8 Hz
+	filter_order = 1
+	# critical_frequencies = [15, 50] #in Hz
+	critical_frequencies =[ 0.5, 8] 	# in Hz
+	FILTER = 'bandpass'				#'bandpass'
 	output = 'sos'
 
 	#design butterworth bandpass filter
 	sos = signal.butter(filter_order, critical_frequencies, FILTER, fs = sampling_frequency, output= output)
 	filtered = signal.sosfilt(sos, data)
+			
 
-	#applying notch filter
-	notch_frequency = 50 	#Hz
-	quality_factor = 30 	# 			-- no reason just copied.
-	
-	#design notch filter 
-	b, a = signal.iirnotch(notch_frequency, quality_factor, sampling_frequency)
-	filtered = signal.lfilter(b, a, filtered)
-	
 	return filtered
 
 data = []
 label = []
-for i in range(len(total_data)):					#gives the number of file
+for i in range(len(total_data)):
 	for j in range(len(total_data[i])):				#choosing the file //as per the index it was necessary
 		for k in range(len(total_data[i][j])):		#chossing the data block in the file
+			for l in range(len(total_data[i][j][k][0][0,:])):
+				total_data[i][j][k][0][:,l] = filter_data(total_data[i][j][k][0][:,l])	#filters all the channel data //please recheck it...
 			data.append(total_data[i][j][k][0])		#recording the data 
 			label.append(total_data[i][j][k][1])	#recording the label
+num_label = len(list(set(label)))
 
 '''
 #data[data_number][sample_number, Channel_number]
@@ -65,35 +101,27 @@ sample_number - number of samples recorded <rows> (variable in length due to dif
 Channel_number - number of channels i.e. 8 <column>
 '''
 
-#visulaize the signals
+#visulaize the signal
 import matplotlib.pyplot as plt 
-plt.plot(data[0][:,0])
+plt.plot(data[0])
 plt.show()
 
+#applying ricker - on test
+# widths = np.arange(1, 50)
+# cwtmatr = signal.cwt(data[0][:,0],signal.ricker, widths)
+# plt.imshow(cwtmatr)
+# plt.show()
 
+#zero padding 
+maximum_length = max(list(map(len, data)))
+print("the maximum_length is : ", maximum_length)
+for i in range(len(data)):
+	pad_width = maximum_length - len(data[i])
+	pad_before_n, pad_after_n = (int(np.floor(pad_width/2)) , int(np.ceil(pad_width/2)))
+	data[i] = np.pad(data[i], ((pad_before_n, pad_after_n), (0, 0)) , constant_values = (0.0, 0.0))
 
-from scipy import signal
-
-#applying bandpass filter
-filter_order = 10
-critical_frequencies = [15, 50] #in Hz
-FILTER = 'bandpass'
-output = 'sos'
-
-#design butterworth bandpass filter
-sos = signal.butter(filter_order, critical_frequencies, FILTER, fs = sampling_frequency, output= output)
-filtered = signal.sosfilt(sos, data[0][:,0])
-plt.plot(filtered)
-plt.show()
-#applying notch filter
-notch_frequency = 50 	#Hz
-quality_factor = 30 	# 			-- no reason just copied.
-
-#design notch filter 
-b, a = signal.iirnotch(notch_frequency, quality_factor, sampling_frequency)
-filtered = signal.lfilter(b, a, filtered)
-plt.plot(filtered)
-plt.show()
+data = np.array(data)
+label = np.array(label)
 
 # TODO 
 #feature extraction
